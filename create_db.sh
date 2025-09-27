e92c3b22431ae8ef0513c0d788c8dc434a71db96
Phil Del Vecchio <philipjdelvecchio@gmail.com>
2025-09-27 12:54:25 -0400

    migrations

--- create_db.sh
+++ create_db.sh
@@ -9,14 +9,20 @@ csv_file="$(mktemp)"
 echo "Downloading data from $csv_url..."
 curl "$csv_url" -o "$csv_file" --create-dirs
 
-echo "Creating sql dump file..."
-mkdir -p "$(dirname "$dump_file")"
-cat ./create_db.sql > "$dump_file"
-
 echo "Converting CSV to SQL..."
-sqlite-utils memory "$csv_file" --csv --table hot100 | sqlite-utils dump --table hot100 - | sed 's/INSERT INTO/INSERT OR IGNORE INTO/g' >> "$dump_file"
+mkdir -p "$(dirname "$dump_file")"
+db_file=$(mktemp)
+sqlite-utils insert "$db_file" hot100 "$csv_file" --csv \
+  --rename peak_pos:peak_position \
+  --rename wks_on_chart:weeks_on_chart \
+  --convert 'id: f"{r["chart_week"]}-{r["performer"]}-{r["title"]}"'
 
-echo "Executing SQL on remote database..."
+# Get the INSERT statements for hot100
+sqlite-utils dump "$db_file" | grep "^INSERT INTO hot100" | sed 's/INSERT INTO/INSERT OR IGNORE INTO/g' > "$dump_file"
+rm "$db_file"
+
+# Append the statement to populate the peaks table
+echo "INSERT OR REPLACE INTO hot100_peaks SELECT performer, title, MIN(current_week) AS peak FROM hot100 GROUP BY 1, 2 ORDER BY 1;" >> "$dump_file"
+
+echo "Inserting to remote DB..."
 npx wrangler d1 execute hot100-db --remote --file="$dump_file"
-
-echo "Database creation finished successfully."
--- migrations/0000_initial_schema.sql
+++ migrations/0000_initial_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS hot100 (
+CREATE TABLE hot100 (
     id TEXT PRIMARY KEY,
     chart_week TEXT,
     current_week INTEGER,
@@ -9,19 +9,14 @@ CREATE TABLE IF NOT EXISTS hot100 (
     weeks_on_chart INTEGER
 );
 
-CREATE VIRTUAL TABLE IF NOT EXISTS hot100_peaks USING fts5(performer, title, peak);
-INSERT OR REPLACE INTO hot100_peaks
-    SELECT performer, title, MIN(current_week) AS peak
-    FROM hot100
-    GROUP BY 1, 2
-    ORDER BY 1;
+CREATE VIRTUAL TABLE hot100_peaks USING fts5(performer, title, peak);
 
-CREATE VIEW IF NOT EXISTS hot100_tracks AS
+CREATE VIEW hot100_tracks AS
     SELECT performer, title, chart_week AS week, current_week AS position
     FROM hot100
     ORDER BY 1, 2, 3;
 
-CREATE VIEW IF NOT EXISTS hot100_weeks AS
+CREATE VIEW hot100_weeks AS
     SELECT chart_week AS week, current_week AS position, performer, title
     FROM hot100
     ORDER BY 1, 2;
