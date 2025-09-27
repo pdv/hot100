ce70fc20864d21414c6309b4f50bac621f40282e
Phil Del Vecchio <philipjdelvecchio@gmail.com>
2025-09-27 13:27:23 -0400

    drop tables

--- create_db.sh
+++ create_db.sh
@@ -13,16 +13,27 @@ echo "Converting CSV to SQL..."
 mkdir -p "$(dirname "$dump_file")"
 db_file=$(mktemp)
 sqlite-utils insert "$db_file" hot100 "$csv_file" --csv \
-  --rename peak_pos:peak_position \
-  --rename wks_on_chart:weeks_on_chart \
-  --convert 'id: f"{r["chart_week"]}-{r["performer"]}-{r["title"]}"'
+  --convert '
+    row["id"] = f"""{row["chart_week"]}-{row["current_week"]}"""
+    row["peak_position"] = row.pop("peak_pos")
+    row["weeks_on_chart"] = row.pop("wks_on_chart")
+    return row
+'
 
-# Get the INSERT statements for hot100
-sqlite-utils dump "$db_file" | grep "^INSERT INTO hot100" | sed 's/INSERT INTO/INSERT OR IGNORE INTO/g' > "$dump_file"
+# Create the dump file
+{
+  echo "DROP TABLE IF EXISTS hot100;"
+  echo "DROP TABLE IF EXISTS hot100_peaks;"
+  echo "DROP VIEW IF EXISTS hot100_tracks;"
+  echo "DROP VIEW IF EXISTS hot100_weeks;"
+  sqlite-utils dump "$db_file" | grep -v "^BEGIN TRANSACTION;" | grep -v "^COMMIT;"
+  echo "CREATE VIRTUAL TABLE hot100_peaks USING fts5(performer, title, peak);"
+  echo "INSERT INTO hot100_peaks (performer, title, peak) SELECT performer, title, MIN(current_week) AS peak FROM hot100 GROUP BY 1, 2 ORDER BY 1;"
+  echo "CREATE VIEW hot100_tracks AS SELECT performer, title, chart_week AS week, current_week AS position FROM hot100 ORDER BY 1, 2, 3;"
+  echo "CREATE VIEW hot100_weeks AS SELECT chart_week AS week, current_week AS position, performer, title FROM hot100 ORDER BY 1, 2;"
+} > "$dump_file"
+
 rm "$db_file"
-
-# Append the statement to populate the peaks table
-echo "INSERT OR REPLACE INTO hot100_peaks SELECT performer, title, MIN(current_week) AS peak FROM hot100 GROUP BY 1, 2 ORDER BY 1;" >> "$dump_file"
 
 echo "Inserting to remote DB..."
 npx wrangler d1 execute hot100-db --remote --file="$dump_file"
